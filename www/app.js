23f5506f1f0a4af5e6b0ca9a2e2d095e1e20350f
Christian Petrov <cpetrov@eclipsesource.com>
2015-07-27 20:41:38 +0200

    Add initial map app example
    Create a map widget on button press. Widget to be implemented.

--- www/app.js
+++ www/app.js
@@ -1,10 +1,17 @@
+tabris.registerWidget("Map", {});
+
 var page = tabris.create("Page", {
   title: "Maps Custom Widget",
   topLevel: true
 });
 
-var map = tabris.create("Map", {
-  layoutData: {left: 10, top: 10, width: 300, height: 600}
+var button = tabris.create("Button", {
+  text: "Show Map",
+  layoutData: {centerX: 0, top: 10}
+}).on("select", function() {
+  var map = tabris.create("Map", {
+    layoutData: {left: 10, top: [button, 10], width: 300, height: 600}
+  }).appendTo(page);
 }).appendTo(page);
 
 page.open();
