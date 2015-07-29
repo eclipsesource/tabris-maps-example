2118fb593df5d4ff88b30e710d4ab1e40b6eb431
Christian Petrov <cpetrov@eclipsesource.com>
2015-07-29 10:34:19 +0200

    Make map shorter to fit phone displays

--- www/app.js
+++ www/app.js
@@ -9,9 +9,11 @@ var button = tabris.create("Button", {
   text: "Show Map",
   layoutData: {centerX: 0, top: 10}
 }).on("select", function() {
+
   var map = tabris.create("Map", {
-    layoutData: {left: 10, top: [button, 10], width: 300, height: 600}
+    layoutData: {left: 10, top: [button, 10], width: 300, height: 400}
   }).appendTo(page);
+
 }).appendTo(page);
 
 page.open();
