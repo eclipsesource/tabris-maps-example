76677ff3c3247aed183ec12d3134c58187cf57b1
Christian Petrov <cpetrov@eclipsesource.com>
2015-07-31 16:54:30 +0200

    Add zoom slider demo page
    Demonstrate programmatically setting the zoom level.

--- www/index.js
+++ www/index.js
@@ -1,8 +1,10 @@
 require("./mapWidget.js");
 var latLngPage = require("./pages/latLngPage");
 var mapTypePage = require("./pages/mapTypePage");
+var zoomPage = require("./pages/zoomPage");
 var drawer = tabris.create("Drawer");
 
 tabris.create("PageSelector").appendTo(drawer);
 latLngPage.create().open();
 mapTypePage.create();
+zoomPage.create();
---
+++ www/pages/zoomPage.js
@@ -0,0 +1,29 @@
+var MIN_ZOOM_VALUE = 2;
+var MAX_ZOOM_VALUE = 21;
+
+exports.create = function() {
+
+  var page = tabris.create("Page", {
+    title: "zoom",
+    topLevel: true
+  });
+
+  var map = tabris.create("Map", {
+    layoutData: {left: 0, right: 0, top: 0, height: 200}
+  }).appendTo(page);
+
+  tabris.create("Slider", {
+    layoutData: {left: 10, top: [map, 10], right: 10},
+    minimum: 0,
+    maximum: 100
+  }).on("change:selection", function(widget, selection) {
+    map.set("zoom", getZoomFromSelection(selection));
+  }).appendTo(page);
+
+  return page;
+
+};
+
+function getZoomFromSelection(selection) {
+  return (selection * MAX_ZOOM_VALUE - MIN_ZOOM_VALUE) / 100 + MIN_ZOOM_VALUE;
+}
