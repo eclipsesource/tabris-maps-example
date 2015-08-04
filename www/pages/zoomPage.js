ec53afbdf661ae44423fdd08455d642569c330eb
Christian Petrov <cpetrov@eclipsesource.com>
2015-08-04 15:47:59 +0200

    Add zoom level label in zoom example
    Done to display current zoom level as zoom level changes.

--- www/pages/zoomPage.js
+++ www/pages/zoomPage.js
@@ -12,17 +12,26 @@ function createExample(page) {
   var map = tabris.create("Map", {
     layoutData: {left: 0, right: 0, top: 0, height: 200}
   }).on("mapReady", function() {
+
     this.set("center", [-33.867, 151.206]);
+
     tabris.create("Slider", {
       layoutData: {left: 10, top: [map, 10], right: 10},
       minimum: 0,
       maximum: 100
     }).on("change:selection", function(widget, selection) {
       map.set("zoom", getZoomFromSelection(selection));
+      zoomLevel.set("text", "Zoom level: <b>" + map.get("zoom") + "</b>").appendTo(page);
     }).appendTo(page);
+
+    var zoomLevel = tabris.create("TextView", {
+      layoutData: {centerX: 0, top: [page.children().last(), 10]},
+      markupEnabled: true
+    }).appendTo(page);
+
   }).appendTo(page);
 }
 
 function getZoomFromSelection(selection) {
-  return (selection * MAX_ZOOM_VALUE - MIN_ZOOM_VALUE) / 100 + MIN_ZOOM_VALUE;
+  return (selection * (MAX_ZOOM_VALUE - MIN_ZOOM_VALUE)) / 100 + MIN_ZOOM_VALUE;
 }
