0bf5395ab1923c5e3c3e473670b6a2d9f09dda53
Christian Petrov <cpetrov@eclipsesource.com>
2015-08-06 16:16:08 +0200

    Add marker example page
    To add a marker, tap on the map. To remove it, tap on the marker.

--- www/app.js
+++ www/app.js
@@ -3,6 +3,7 @@ var mapTypePage = require("./pages/mapTypePage");
 var zoomPage = require("./pages/zoomPage");
 var maptapPage = require("./pages/maptapPage");
 var maplongpressPage = require("./pages/maplongpressPage");
+var markerPage = require("./pages/markerPage");
 var drawer = tabris.create("Drawer");
 
 tabris.create("PageSelector").appendTo(drawer);
@@ -11,3 +12,4 @@ mapTypePage.create();
 zoomPage.create();
 maptapPage.create();
 maplongpressPage.create();
+markerPage.create();
---
+++ www/pages/markerPage.js
@@ -0,0 +1,51 @@
+exports.create = function() {
+  return tabris.create("Page", {
+    title: "Marker",
+    topLevel: true
+  }).once("appear", createExample);
+};
+
+function createExample(page) {
+
+  var markersCount = 0;
+  var markerIds = [];
+
+  var map = tabris.create("Map", {
+    layoutData: {left: 0, right: 0, top: 0, height: 200}
+  }).on("ready", mapReady).appendTo(page);
+
+  var markerIdsTextView = tabris.create("TextView", {
+    markupEnabled: true,
+    layoutData: {left: 10, top: [map, 10], right: 10},
+    text: "Active marker ids: <b>?</b>"
+  }).appendTo(page);
+
+  function mapReady() {
+    this.set("center", [-33.867, 151.206]);
+    this.on("tap", function(widget, latLng) {
+      var marker = this.createMarker({latLng: latLng});
+      marker.set("id", ++markersCount);
+      markerIds.push(marker.get("id"));
+      updateMarkerIdsTextView();
+      marker.on("tap", removeMarker);
+    });
+  }
+
+  function removeMarker() {
+    this.dispose();
+    var markerId = markerIds.indexOf(this.get("id"));
+    if (markerId) {
+      markerIds.splice(markerId, 1);
+    }
+    updateMarkerIdsTextView();
+  }
+
+  function updateMarkerIdsTextView() {
+    var markerIdsString = markerIds.join(", ");
+    markerIdsTextView.set("text",
+      "Active marker ids: <b>" + (markerIdsString ? markerIdsString : "?") + "</b>");
+  }
+
+  updateMarkerIdsTextView();
+
+}
