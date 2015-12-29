86b3f0ac0d0fc73ef97a1115e1a896fed1128b55
Johannes Eickhold <jeick@eclipsesource.com>
2015-12-29 10:11:12 +0100

    Fix display of center coords in pan event example

--- www/pages/mappanPage.js
+++ www/pages/mappanPage.js
@@ -6,20 +6,28 @@ exports.create = function() {
 };
 
 function createExample(page) {
+  var initialLatLng = [-33.867, 151.206];
+
   var map = tabris.create("ESMap", {
     layoutData: {left: 0, right: 0, top: 0, height: 200}
   }).on("ready", function() {
-    this.set("center", [-33.867, 151.206]);
-    this.on("pan", function(widget, latLng) {
-      var text = "Map tap position:<br/>" +
-        "Latitude: <b>" + latLng[0] + "</b><br/>" +
-        "Longitude: <b>" + latLng[1] + "</b>";
-      tapEventDisplay.set("text", text);
+    this.set("center", initialLatLng);
+    this.on("pan", function(widget, target) {
+      updatePanEventDisplay(target);
     });
   }).appendTo(page);
 
-  var tapEventDisplay = tabris.create("TextView", {
-    markupEnabled: true,
-    layoutData: {left: 10, top: [map, 10], right: 10}
+  var panEventDisplay = tabris.create("TextView", {
+    layoutData: {left: 10, top: [map, 10], right: 10},
+    markupEnabled: true
   }).appendTo(page);
+
+  function updatePanEventDisplay(target) {
+    var text = "Center of map:<br/>" +
+      "Latitude: <b>" + target[0] + "</b><br/>" +
+      "Longitude: <b>" + target[1] + "</b>";
+    panEventDisplay.set("text", text);
+  }
+
+  updatePanEventDisplay(initialLatLng);
 }
