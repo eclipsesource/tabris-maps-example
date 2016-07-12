b6aec7f01ece2a650ea65ca83a0abd73213c3b85
Moritz Post <mpost@eclipsesource.com>
2016-07-12 17:07:53 +0200

    Update property "center" example to use "position"

--- www/pages/centerPage.js
+++ www/pages/centerPage.js
@@ -1,6 +1,6 @@
 exports.create = function() {
   return tabris.create("Page", {
-    title: "property: center",
+    title: "Property 'center'",
     topLevel: true
   }).once("appear", createExample);
 };
@@ -9,7 +9,7 @@ function createExample(page) {
   var map = tabris.create("ESMap", {
     layoutData: {left: 0, right: 0, top: 0, height: 200}
   }).on("ready", function() {
-    this.set("center", [-33.867, 151.206]);
+    this.set("position", [-33.867, 151.206]);
     this.set("zoom", 13);
   }).appendTo(page);
 
@@ -20,7 +20,7 @@ function createExample(page) {
   var blacktown = tabris.create("Button", {
     text: "Go to Blacktown"
   }).on("select", function() {
-    map.set("center", [-33.769, 150.907]);
+    map.set("camera", {position: [-33.769, 150.907]});
     map.set("zoom", 14);
   }).appendTo(locationButtons);
 
@@ -28,23 +28,23 @@ function createExample(page) {
     text: "Go to Sydney",
     layoutData: {left: [blacktown, 10], top: 0}
   }).on("select", function() {
-    map.set("center", [-33.867, 151.206]);
+    map.set("position", [-33.867, 151.206]);
     map.set("zoom", 13);
   }).appendTo(locationButtons);
 
   var center = tabris.create("Button", {
-    text: "Show Center Position",
+    text: "Show position",
     layoutData: {centerX: 0, top: [locationButtons, 10]}
   }).on("select", function() {
-    var center = map.get("center");
-    centerPosition.set("text", "Center position:<br/>" +
+    var center = map.get("position");
+    centerPosition.set("text", "Position:<br/>" +
       "Latitude: <b>" + center[0] + "</b><br/>" +
       "Longitude: <b>" + center[1] + "</b>");
   }).appendTo(page);
 
   var centerPosition = tabris.create("TextView", {
     layoutData: {centerX: 0, top: [center, 10]},
-    text: "Center Position: <b>?</b>",
+    text: "Position: <b>?</b>",
     markupEnabled: true
   }).appendTo(page);
 }
